d3358754311bbcb6845694af00f33f5f5d788727
Nishat199 <nishatanjumdhaka18@gamil.com>
2020-10-27 10:28:33 -0700

    text

--- html.php
+++ html.php
@@ -1,11 +1,17 @@
 <?php
 $gender="";
 $err_gender="";
-$hobbies="";
-$err_profession="";
 $name="";
 $err_name="";
+$has_error=false;
 
+if ( isset ($_POST["gender"])){
+	$err_gender="gender required";
+	$has_error=true;
+}
+else{
+	$gender=$_POST["gender"];
+}
 
 ?>
 <html>
@@ -16,37 +22,49 @@ $err_name="";
 <hr>
 <form action="" method="post">
 <fieldset>
-<legend>Club Member Registration</legend>
+<legend align="center">Club Member Registration</legend>
 <table>
 <tr>
-<td>Name:</td>
+<td align="right">Name:</td>
 <td><input type="text" name=""></td>
 </tr>
 <tr>
-<td>Username:</td>
+<td align="right">Username:</td>
 <td><input type="text"></td>
 </tr>
 <tr>
-<td>Password:</td>
+<td align="right">Password:</td>
 <td><input type="password" name="password"></td>
 </tr>
 <tr>
-<td>Confirm Password:</td>
+<td align="right">Confirm Password:</td>
 <td><input type=" Confirm Password" name="Confirm password"></td>
 </tr>
 <tr>
-<td>Email:</td>
+<td align="right">Email:</td>
 <td><input type="text"></td>
 </tr>
 <tr>
-<td>Address: </td>
-<td><input type="text" value="street address">
+<td align="right">Phone:</td>
+<td><input type="text" placeholder="code" size="3"> -
+<input type="text" placeholder="Number" size="9">
+</td></tr>
+<tr>
+<td align="right">Address: </td>
+<td><input type="text" placeholder="Street Address">
 </td>
 </tr>
 <tr>
-<td align="right">Birth Date:</td>
-<td> <select>>
-<option selected disabled>Day</option> 
+<td></td>
+<td><input type="text" placeholder="City" size="6"> -
+<input type="text" placeholder="State" size="6"></td></tr>
+<tr><td></td>
+<td><input type="text" placeholder="Postal/Zip code">
+</td></tr>
+<tr>
+<td align ="right">Birth Date:</td>
+<td> <select>
+<option selected>Day</option> 
 <?php 
 for ($i-1;$i<-31;$i++){
 	echo "<option>",$i, "<option>";
@@ -61,8 +79,28 @@ for ($i-1;$i<-31;$i++){
 </select>
 </td></tr>
 <tr>
-<td>Gender:</td>
+<td align="right">Gender:</td>
 <td><input type="radio" name="gender" value="Male">Male
 <input type="radio" name="gender" value="Female">Female
 </td>
 </tr>
+<tr>
+<td align="right">Where did you hear about us?</td>
+<td><input type="checkbox" name="Where did you hear about us[]">A friend or Colleague<br>
+
+<input type="checkbox" name="Where did you hear about us[]">Google<br>
+
+<input type="checkbox" name="Where did you hear about us[]">Blog Post<br>
+
+<input type="checkbox" name="Where did you hear about us[]">News Article<br>
+</td></tr>
+<tr>
+<td align="right">Bio:</td>
+<td><textarea></textarea>
+</td></tr>
+<tr>
+<td align="right"><input type="submit" value="register"></td>
+<tr>
+</table>
+
+
